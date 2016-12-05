522e4779b4954d3f0428d88b18a02076cd5c3857
Andrew Wang <andidiwang@yahoo.com>
2016-12-04 21:06:37 -0800

    Bugfixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanJBlueAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanJBlueAutoOp.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Plan C: Blue", group="Plan C")
+@Autonomous(name="Plan J: Blue", group="Plan J")
 public class PlanJBlueAutoOp extends PlanJRedAutoOp {
 
     @Override
