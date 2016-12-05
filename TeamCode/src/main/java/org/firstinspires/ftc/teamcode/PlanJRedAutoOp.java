44c917f7572a210627e89e1bcfab8fddbadbb327
Andrew Wang <andidiwang@yahoo.com>
2016-12-04 21:10:17 -0800

    Bugfixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanJRedAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanJRedAutoOp.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Plan C: Red", group="Plan C")
+@Autonomous(name="Plan J: Red", group="Plan J")
 public class PlanJRedAutoOp extends VortexAutoOp {
 
     int leftArmHitBallPosition = 400;
